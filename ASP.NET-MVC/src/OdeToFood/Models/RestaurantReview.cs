a85f12d5b98671b3d85ae6bf62d94fa3881b4bee
anton.szorad <anton.szorad@gmail.com>
2016-05-09 16:12:26 +0200

    Added some Validation Annotations

--- ASP.NET-MVC/src/OdeToFood/Models/RestaurantReview.cs
+++ ASP.NET-MVC/src/OdeToFood/Models/RestaurantReview.cs
@@ -1,15 +1,20 @@
-﻿namespace OdeToFood.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models
 {
     public class RestaurantReview
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(1024)]
         public string Name { get; set; }
 
         public string City { get; set; }
 
         public string Country { get; set; }
 
+        [Range(1,10)]
         public int Rating { get; set; }
     }
 }

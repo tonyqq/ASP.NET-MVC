4f57037b9034cfbd0fae7e008fd898033e4477e7
anton.szorad <anton.szorad@gmail.com>
2016-05-11 14:03:58 +0200

    Added migration

--- ASP.NET-MVC/src/OdeToFood/Migrations/20160511114345_v1.Designer.cs
+++ ASP.NET-MVC/src/OdeToFood/Migrations/20160511114345_v1.Designer.cs
@@ -8,9 +8,10 @@ using OdeToFood.Models;
 namespace OdeToFood.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160511114345_v1")]
+    partial class v1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
